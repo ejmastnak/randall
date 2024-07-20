dc88a3a855c59700ae6683e45bb8b6bb620df880
Elijan Mastnak <git@ejmastnak.com>
2024-07-20 09:44:21 +0200

    feat: fade mobile menu when leaving page
    Closes the mobile menu when navigating to a new component, and
    adds a fade transition when showing/hiding the menu.

--- src/Shared/Header.vue
+++ src/Shared/Header.vue
@@ -16,7 +16,7 @@ const mobileMenuShowing = ref(false)
       <div class="hidden md:flex justify-between items-center pt-3 pb-5 px-4">
         <HeaderLink route="/">Home</HeaderLink>
         <HeaderLink route="/about">About</HeaderLink>
-        <HeaderLink route="/">Services</HeaderLink>
+        <HeaderLink route="/services">Services</HeaderLink>
         <HeaderLink route="/" class="font-['Latin_Modern_Roman'] !text-3xl tracking-wide hover:text-blue-800">
           <div class="text-center tracking-wider">
             <p>Artisanal</p>
@@ -41,17 +41,30 @@ const mobileMenuShowing = ref(false)
 
     </div>
 
-    <div v-show="mobileMenuShowing" class="absolute right-0 w-fit bg-white border-x border-b border-gray-300">
-      <div class="relative px-8 pt-6 pb-8 text-left w-fit mx-auto space-y-4">
-        <HeaderLink class="border-b" route="/">Home</HeaderLink>
-        <HeaderLink class="border-b" route="/about">About</HeaderLink>
-        <HeaderLink class="border-b" route="/">Services</HeaderLink>
-        <HeaderLink class="border-b" route="/">Portfolio</HeaderLink>
-        <HeaderLink class="border-b" route="/testimonials">Testimonials</HeaderLink>
-        <HeaderLink class="border-b" route="/contact">Contact</HeaderLink>
-      </div>
-    </div>
-
+    <Transition>
+      <div v-show="mobileMenuShowing" class="absolute right-0 w-fit bg-white border-x border-b border-gray-300">
+          <div class="relative px-8 pt-6 pb-8 text-left w-fit mx-auto space-y-4">
+            <HeaderLink @click="mobileMenuShowing = false" class="border-b" route="/">Home</HeaderLink>
+            <HeaderLink @click="mobileMenuShowing = false" class="border-b" route="/about">About</HeaderLink>
+            <HeaderLink @click="mobileMenuShowing = false" class="border-b" route="/services">Services</HeaderLink>
+            <HeaderLink @click="mobileMenuShowing = false" class="border-b" route="/">Portfolio</HeaderLink>
+            <HeaderLink @click="mobileMenuShowing = false" class="border-b" route="/testimonials">Testimonials</HeaderLink>
+            <HeaderLink @click="mobileMenuShowing = false" class="border-b" route="/contact">Contact</HeaderLink>
+          </div>
+        </div>
+    </Transition>
 
   </div>
 </template>
+
+<style>
+  .v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
