ebdbe6db6ab407cdd383a1b5be93ab622be56e2a
Elijan Mastnak <git@ejmastnak.com>
2024-07-21 18:58:17 +0200

    feat: add a SimpleImagePanel component

---
+++ src/Components/SimpleImagePanel.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({
+  reverse: Boolean,
+  imgFile: String,
+  imgAlt: String,
+  imgCaption: String,
+  imgClasses: {
+    type: String,
+    default: "",
+  },
+  captionClasses: {
+    type: String,
+    default: "",
+  },
+})
+</script>
+<template>
+  <div
+    class="md:flex md:gap-x-6"
+    :class="reverse ? 'md:flex-row-reverse' : ''"
+  >
+    <div class="mb-4 md:mb-0 shrink-0">
+      <img 
+      :src="'/src/assets/img/' + imgFile"
+      :alt="imgAlt"
+      class="mx-auto my-auto object-cover rounded-md"
+      :class="imgClasses"
+    />
+      <p v-if="imgCaption" :class="captionClasses" class="text-base text-gray-700 text-center mx-auto">{{imgCaption}}</p>
+    </div>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
