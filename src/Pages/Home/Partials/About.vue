3e4455ee41d54fbe2bbeb15f969353a6f32e365b
Elijan Mastnak <git@ejmastnak.com>
2024-07-23 00:24:37 +0200

    fix: add missing years in business

--- src/Pages/Home/Partials/About.vue
+++ src/Pages/Home/Partials/About.vue
@@ -18,7 +18,7 @@
 
         <p class="mt-5 max-w-2xl">
           Randall has been woodworking since high school and carries over three decades of professional experience in carpentry and adjacent trades.
-          After an N-year detour working as X for Bank of America in the 20N0s, where he gained valuable experience in management, administration, and business operations, he opened Artisanal Restoration and has spent N successful years serving historic homes in the Central New Jersey area.
+          After an N-year detour working as X for Bank of America in the 20N0s, where he gained valuable experience in management, administration, and business operations, he opened Artisanal Restoration and has spent 17 successful years serving historic homes in the Central New Jersey area.
         </p>
 
         <RouterLink to="/about" class="w-fit block mt-3 md:mt-8 mx-auto rounded-md border border-gray-600 px-4 py-2 text-xl hover:bg-gray-50 hover:border-gray-900 ">
